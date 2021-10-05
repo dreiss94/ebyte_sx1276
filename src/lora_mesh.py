4f35c1cbc0cacf2ac381c51a6406f2c0043721e7
dreiss94 <daniel.reiss.94@gmail.com>
2021-10-05 17:38:37 +0100

    adjust controllers counter

--- src/lora_mesh.py
+++ src/lora_mesh.py
@@ -193,7 +193,7 @@ def send_hello(advertising: bool):
         every 10th message is on rendez-vous channel at 0.3kbps 
     """
     counter = random.randint(1,20)
-    if controller == myAddress and stop_increasing == True:
+    if controller == myAddress: # and stop_increasing == True:
         counter = 10
     start = counter
     while not stop_hello.is_set():
